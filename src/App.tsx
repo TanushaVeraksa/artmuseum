414a09efd7bf79b251c5cbb087cbd97041aa7cdd
TanushaVeraksa <veraksa161@gmail.com>
2024-07-19 23:59:40 +0300

    add hook and fetch arts for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { Outlet } from "react-router";
 import NavBar from "./components/NavBar";
+import { useEffect } from 'react';
+import { useAppDispatch } from './hooks/redux';
+import { fetchArts } from './store/actions/ActionCreators';
 
 function App() {
+
+  const dispatch = useAppDispatch();
+  useEffect(()=> {
+    dispatch(fetchArts())
+  }, [])
 
   return <>
   <NavBar/>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import reportWebVitals from './reportWebVitals';
 import { router } from './routes/routes';
 import { RouterProvider } from 'react-router-dom';
 import {createGlobalStyle} from 'styled-components';
+import {setupStore} from './store/store';
+import { Provider } from 'react-redux';
 
 const Global = createGlobalStyle`
 * {
@@ -14,6 +16,8 @@ const Global = createGlobalStyle`
 }
 `
 
+const store = setupStore();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -22,7 +26,9 @@ root.render(
   <>
   <Global/>
   <React.StrictMode>
+  <Provider store =  {store}>
   <RouterProvider router={router}/>
+  </Provider>
   </React.StrictMode>
   </>
 );
