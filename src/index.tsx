52c9a6b967af14e9595a184ef8f7aeb03b96709a
TanushaVeraksa <veraksa161@gmail.com>
2024-07-19 23:37:03 +0300

    add global style

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { router } from './routes/routes';
 import { RouterProvider } from 'react-router-dom';
+import {createGlobalStyle} from 'styled-components';
 
+const Global = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Lexend Deca';
+}
+`
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +20,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <>
+  <Global/>
   <React.StrictMode>
   <RouterProvider router={router}/>
   </React.StrictMode>
