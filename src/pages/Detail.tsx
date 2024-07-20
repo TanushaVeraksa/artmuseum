ce4d236714abd3e4a81b1601fea63815fe3aa096
TanushaVeraksa <veraksa161@gmail.com>
2024-07-20 12:34:11 +0300

    add button in detail page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,34 +1,64 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { useAppSelector, useAppDispatch } from '../hooks/redux';
 import { fetchArt } from '../store/actions/ActionCreator';
+import { favoriteSlice } from '../store/reducers/FavoriteSlice';
 import Flex from '../styles/Flex';
 import CardText from '../styles/CardText';
 import Span from '../styles/Span';
+import Button from '../styles/Button';
 import { ReactComponent as Museum } from '../assets/museum.svg';
+import { ReactComponent as VectorUnactive } from '../assets/bookmark.svg';
+import { IArt } from '../models/IArt';
+
 
 const Detail = () => {
   const {art, isLoading, error} = useAppSelector(state => state.artReducer);
-  const {id} = useParams();
+  const {id}= useParams();
+  const idArt: number = Number(id);
   const {title, artist_title, image_id, department_title, dimensions, place_of_origin, credit_line, date_display} = art;
-
   const dispatch = useAppDispatch();
+  const {favorites} = useAppSelector(state => state.favoritesReducer);
+
+  const existing = () => {
+    const existFavorite = favorites.findIndex(f => f.id === art.id);
+    return existFavorite < 0 ? false : true;
+  }
+
+  const [isClick, setIsClick] = useState(existing());
 
   useEffect(() => {
-      dispatch(fetchArt(id))
+     dispatch(fetchArt(idArt))
   }, [])
+
+  const handleAdd = (favor: IArt) => { 
+    dispatch(favoriteSlice.actions.addFavorite(favor));
+    setIsClick(s => !s)   
+  }
+
+  const handleDelete = (favor: IArt) => {
+    dispatch(favoriteSlice.actions.removeFavorite(favor));
+    setIsClick(s => !s)   
+  }
 
 return (
   <Flex direction = 'column' justify="center" align="center" padding='120px 0'>
     <Flex>
     {isLoading && <h1>Loading...</h1>}
     {error && <h1>{error}</h1>}
-      <div style={{overflow: 'hidden', width: '400px', height: '500px'}}>
-        {image_id ? 
-          <img style={{objectFit:'cover', width:'100%', height:'100%'}} src={`https://www.artic.edu/iiif/2/${image_id}/full/200,/0/default.jpg`} alt={title}/>
-          :
-          <Museum style={{objectFit:'cover', width:'100%', height:'100%'}}/>
-        }
+        <div style={{overflow: 'hidden', width: '400px', height: '500px',  position: 'relative', zIndex: 0}}>
+          {image_id ? 
+            <img style={{objectFit:'cover', width:'100%', height:'100%'}} src={`https://www.artic.edu/iiif/2/${image_id}/full/200,/0/default.jpg`} alt={title}/>
+            :
+            <Museum style={{objectFit:'cover', width:'100%', height:'100%'}}/>
+          }
+          <div style={{position: 'absolute', top:16, right:16, zIndex: 1}}>
+          {isClick ? 
+            <Button color="#FBD7B24D" onClick={() => handleDelete(art)}><VectorUnactive/></Button> 
+          : 
+            <Button onClick={() => handleAdd(art)}><VectorUnactive/></Button>
+          } 
+        </div>
       </div>
       <Flex justify="space-between" direction="column" margin="0 0 0 3em" max="640px">
         <Flex align="flex-start" direction="column">
